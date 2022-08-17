9d34e1f33e0d82fbb4fccf1535b7f4363bd5c622
Matthias Geisler <matthias.geisler@data4life.care>
2022-08-17 08:44:29 +0200

    Bump Updates
    AGP 7.2.2
    Kotlin 1.7.10
    mockito 4.7.0
    jsonassert 1.5.1

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -36,11 +36,11 @@ dependencies {
     implementation(care.data4life.gradle.fhir.dependency.GradlePlugin.android)
 
     // dependency-updates.gradle.kts
-    implementation("com.github.ben-manes:gradle-versions-plugin:0.39.0")
+    implementation("com.github.ben-manes:gradle-versions-plugin:0.42.0")
     // download-scripts.gradle.kts
-    implementation("de.undercouch:gradle-download-task:4.1.2")
+    implementation("de.undercouch:gradle-download-task:5.1.1")
     // publishing.gradle.kts
-    implementation("org.eclipse.jgit:org.eclipse.jgit:5.11.0.202103091610-r")
+    implementation("org.eclipse.jgit:org.eclipse.jgit:6.2.0.202206071550-r")
     // versioning.gradle.kts
-    implementation("care.data4life.gradle.gitversion:gradle-git-version:0.12.4-d4l")
+    implementation("com.palantir.gradle.gitversion:gradle-git-version:0.15.0")
 }
